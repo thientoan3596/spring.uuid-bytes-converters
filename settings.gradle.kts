1e668e32e6950398b22c63758e3d49117e0bd19d
thientoan3596 <toanlt3596@gmail.com>
2025-04-10 12:42:24 +0700

    Fix settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,4 +3,4 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 }
 
-rootProject.name = "uuid-bytes-converter"
+rootProject.name = "spring.uuid-bytes-converter"
